d898422dc3156ed3d9ad66dcee41d72decc430e9
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-22 16:40:30 +0700

    Update plugins.lua

--- lua/Plugs/plugins.lua
+++ lua/Plugs/plugins.lua
@@ -2,7 +2,7 @@ local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({
+    packer_bootstrap = fn.system({
       'git',
       'clone',
       '--depth',
@@ -19,9 +19,32 @@ local ensure_packer = function()
   return false
 end
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
+
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
+return packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   --LSP
   use 'neovim/nvim-lspconfig'
