8fe75e7a9d3619758cb4788ce9b142ed450195d0
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-22 16:14:55 +0700

    Update plugins.lua

--- lua/Plugs/plugins.lua
+++ lua/Plugs/plugins.lua
@@ -17,6 +17,13 @@ local ensure_packer = function()
   return false
 end
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
     --for not show error massage
     local status_ok, packer = pcall(require, "packer")
     if not status_ok then
