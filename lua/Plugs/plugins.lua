e7e826c42581f47b12a72937378066bc8a441536
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-22 17:10:46 +0700

    Update plugins.lua

--- lua/Plugs/plugins.lua
+++ lua/Plugs/plugins.lua
@@ -1,48 +1,8 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({
-      'git',
-      'clone',
-      '--depth',
-      '1',
-      'https://github.com/wbthomason/packer.nvim',
-      install_path
-    })
-    print('installing packer and configure it.....')
-    vim.cmd [[packadd packer.nvim]]
-    vim.cmd "bw | silent! MasonInstallAll" -- close packer window
-    require("packer").loader "nvim-treesitter"
-    return true
-  end
-  return false
-end
-
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
 	return
 end
-
--- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
-
-
-local packer_bootstrap = ensure_packer()
 
 return packer.startup(function(use)
   use 'wbthomason/packer.nvim'
@@ -75,7 +35,8 @@ return packer.startup(function(use)
        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
   use 'folke/tokyonight.nvim'
-  if packer_bootstrap then
+  if status_ok then
     require('packer').sync()
+      packer.init(init_options)
   end
 end)
