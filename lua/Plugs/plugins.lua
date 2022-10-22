b849b128c171a2ac404ac2fcd3a4d8c4d80e34c6
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-22 16:16:36 +0700

    Add files via upload

---
+++ lua/Plugs/plugins.lua
@@ -0,0 +1,53 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+      'git',
+      'clone',
+      '--depth',
+      '1',
+      'https://github.com/wbthomason/packer.nvim',
+      install_path
+    })
+    print('installing packer and configure it.....')
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  --LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'hrsh7th/cmp-nvim-lsp'
+  --CMP
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'rafamadriz/friendly-snippets'
+  use 'onsails/lspkind-nvim'
+  --Good exoerience
+  use 'numToStr/Comment.nvim'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
+  use 'nvim-lualine/lualine.nvim'
+  use {'akinsho/bufferline.nvim', tag = "v3.*"}
+  --Syntax highlighting && colorscheme
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use 'folke/tokyonight.nvim'
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
