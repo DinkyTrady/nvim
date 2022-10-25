76c6ac659a8f23adc5761343008e747d0e933ac8
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-25 21:11:14 +0700

    Update plugins.lua

--- lua/Plugs/plugins.lua
+++ lua/Plugs/plugins.lua
@@ -1,3 +1,4 @@
+local fn = vim.fn
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 ---@diagnostic disable-next-line: missing-parameter
 if fn.empty(fn.glob(install_path)) > 0 then
