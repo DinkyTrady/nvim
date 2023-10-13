05d8bb821af373f46bee53321503bee2908d8d60
DinkyTrady <rnys673@gmail.com>
2023-10-13 14:16:05 +0700

    adding footer in dashboard [alpha]

--- lua/configs/alpha.lua
+++ lua/configs/alpha.lua
@@ -45,7 +45,17 @@ function M.setup()
 
   db.opts.opts.noautocmd = true
   alpha.setup(db.opts)
-  -- 
+
+  vim.api.nvim_create_autocmd("User", {
+    once = true,
+    pattern = "LazyVimStarted",
+    callback = function()
+      local stats = require("lazy").stats()
+      local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+      db.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+      pcall(vim.cmd.AlphaRedraw)
+    end,
+  })
 end
 
 return M
