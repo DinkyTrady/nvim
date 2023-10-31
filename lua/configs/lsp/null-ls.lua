a81392100fa457fa398c2484a0f5600a7b8cf5c1
DinkyTrady <rnys673@gmail.com>
2023-10-31 17:27:55 +0700

    disable null-ls. if want to use it please uncomment it

--- lua/configs/lsp/null-ls.lua
+++ lua/configs/lsp/null-ls.lua
@@ -15,25 +15,25 @@ null_ls.setup({
     -- diagnostic.clang_check.with({
     --   diagnostics_format = "[check] #{m}\n(#{c})",
     -- }),
-    diagnostic.eslint.with({
-      diagnostics_format = "[eslint] #{m}\n(#{c})",
-    }),
-    format.prettier,
+    -- diagnostic.eslint.with({
+    --   diagnostics_format = "[eslint] #{m}\n(#{c})",
+    -- }),
+    -- format.prettier,
     -- format.clang_format,
     -- format.eslint_d,
-    format.stylua,
+    -- format.stylua,
   },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.7, you should use vim.lsp.buf.formatting_sync() instead
-          vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 1500 })
-        end,
-      })
-    end
-  end,
+  -- on_attach = function(client, bufnr)
+  --   if client.supports_method("textDocument/formatting") then
+  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --     vim.api.nvim_create_autocmd("BufWritePre", {
+  --       group = augroup,
+  --       buffer = bufnr,
+  --       callback = function()
+  --         -- on 0.7, you should use vim.lsp.buf.formatting_sync() instead
+  --         vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 1500 })
+  --       end,
+  --     })
+  --   end
+  -- end,
 })
