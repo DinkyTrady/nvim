9835bc0731e1cb58713cbedfd92bbf0607397d19
m <m>
2023-11-22 16:25:42 +0700

    delete mason-lspconfig file and setup mason

--- lua/configs/lsp/mason.lua
+++ lua/configs/lsp/mason.lua
@@ -4,33 +4,37 @@ M.config = {
   "williamboman/mason.nvim",
   cmd = require("core.commands").mason,
   config = function()
-    require("configs.lsp.mason").setup()
+    local status, mason = pcall(require, "mason")
+    if not status then
+      return
+    end
+
+    mason.setup({
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
   end,
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     cmd = require("core.commands").mason_lsp,
     config = function()
-      require("configs.lsp.mason-lspconfig").setup()
+      local masonlsp_ok, masonlsp = pcall(require, "mason-lspconfig")
+      if not masonlsp_ok then
+        return
+      end
+
+      masonlsp.setup({
+        ensure_installed = { "html", "cssls", "tsserver", "emmet_ls", "jsonls", "eslint" },
+        automatic_installation = false,
+      })
     end,
   },
 }
-
-function M.setup()
-  local status, mason = pcall(require, "mason")
-  if not status then
-    return
-  end
-
-  mason.setup({
-    ui = {
-      border = "rounded",
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗",
-      },
-    },
-  })
-end
 
 return M
