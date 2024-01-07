51cc303e0194e0485ae7c274e1e6a8411d21cf3d
DinkyTrady <DinkyTrady@kyra.com>
2024-01-07 10:58:52 +0700

    Revert "Revert "remove unneded plugins""
    This reverts commit ac7b79e08c783c08fc027489efcac054961200a5.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -63,6 +63,72 @@ return {
     },
   },
   {
+    "SmiteshP/nvim-navic",
+    lazy = true,
+    event = "LazyFile",
+    init = function()
+      vim.g.navic_silence = true
+      require("lazyvim.util").lsp.on_attach(function(client, buffer)
+        if client.supports_method("textDocument/documentSymbol") then
+          require("nvim-navic").attach(client, buffer)
+        end
+      end)
+    end,
+    opts = function()
+      _G.Winbar = {}
+
+      local fn = vim.fn
+      vim.api.nvim_set_hl(0, "edited", { fg = "#9ece6a" })
+
+      local sep = function()
+        ---@diagnostic disable-next-line: deprecated
+        if require("nvim-navic").get_location() ~= "" then
+          return "%#NavicSeparator#" .. " > " .. "%*"
+        end
+
+        return ""
+      end
+
+      local fileName = function()
+        if vim.bo.filetype ~= "alpha" then
+          return "%#NavicText#" .. fn.expand("%:t")
+        end
+
+        return ""
+      end
+
+      local function modified()
+        if vim.bo.modified then
+          return "%#edited#" .. " ●" .. "%*"
+        elseif vim.bo.modifiable == false or vim.bo.readonly == true then
+          return " "
+        end
+
+        return ""
+      end
+
+      _G.Winbar.setup = function()
+        return table.concat({
+          " ",
+          fileName(),
+          modified(),
+          sep(),
+          require("nvim-navic").get_location(),
+        })
+      end
+
+      vim.opt.winbar = "%{%v:lua.Winbar.setup()%}"
+
+      return {
+        click = true,
+        highlight = true,
+        depth_limit = 5,
+        icons = require("lazyvim.config").icons.kinds,
+        lazy_update_context = true,
+      }
+    end,
+  },
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-telescope/telescope-file-browser.nvim",
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,32 +25,6 @@ return {
       },
     },
   },
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   optional = true,
-  --   opts = function(_, opts)
-  --     local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
-  --
-  --     -- local formatter = { "prettier", "stylua", "shfmt" }
-  --     -- local nls = require("null-ls")
-  --
-  --     opts.on_attach = function(client, bufnr)
-  --       if client.supports_method("textDocument/formatting") then
-  --         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-  --         vim.api.nvim_create_autocmd("BufWritePre", {
-  --           group = augroup,
-  --           buffer = bufnr,
-  --           callback = function()
-  --             -- on 0.7, use vim.lsp.buf.formatting_sync() instead
-  --             vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 1500 })
-  --           end,
-  --         })
-  --       end
-  --     end
-  --
-  --     return opts
-  --   end,
-  -- },
   {
     "j-hui/fidget.nvim",
     tag = "v1.0.0",
